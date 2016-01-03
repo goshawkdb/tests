e53fb94322a9f19433718965d27deef65ccddc6f
Matthew Sackman <matthew@goshawkdb.io>
2016-01-03 10:46:10 +0000

    TLS transition. Ref T3.
    --HG--
    branch : T3

--- helpers.go
+++ helpers.go
@@ -12,8 +12,35 @@ import (
 )
 
 const (
-	testUsername = "test"
-	testPassword = "test"
+	clusterCertPEM = `-----BEGIN CERTIFICATE-----
+MIIBxzCCAW2gAwIBAgIIQqu37k6KPOIwCgYIKoZIzj0EAwIwOjESMBAGA1UEChMJ
+R29zaGF3a0RCMSQwIgYDVQQDExtDbHVzdGVyIENBIFJvb3QgQ2VydGlmaWNhdGUw
+IBcNMTYwMTAzMDkwODE2WhgPMjIxNjAxMDMwOTA4MTZaMDoxEjAQBgNVBAoTCUdv
+c2hhd2tEQjEkMCIGA1UEAxMbQ2x1c3RlciBDQSBSb290IENlcnRpZmljYXRlMFkw
+EwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEjHBXt+0n477zVZHTsGgu9rLYzNz/WMLm
+l7/KC5v2nx+RC9yfkyfBKq8jJk3KYoB/YJ7s8BH0T456/+nRQIUo7qNbMFkwDgYD
+VR0PAQH/BAQDAgIEMA8GA1UdEwEB/wQFMAMBAf8wGQYDVR0OBBIEEL9sxrcr6QTw
+wk5csm2ZcfgwGwYDVR0jBBQwEoAQv2zGtyvpBPDCTlyybZlx+DAKBggqhkjOPQQD
+AgNIADBFAiAy9NW3zE1ACYDWcp+qeTjQOfEtED3c/LKIXhrbzg2N/QIhANLb4crz
+9ENxIifhZcJ/S2lqf49xZZS91dLF4x5ApKci
+-----END CERTIFICATE-----`
+	clientCertAndKeyPEM = `-----BEGIN CERTIFICATE-----
+MIIBszCCAVmgAwIBAgIIfOmxD9dF8ZMwCgYIKoZIzj0EAwIwOjESMBAGA1UEChMJ
+R29zaGF3a0RCMSQwIgYDVQQDExtDbHVzdGVyIENBIFJvb3QgQ2VydGlmaWNhdGUw
+IBcNMTYwMTAzMDkwODUwWhgPMjIxNjAxMDMwOTA4NTBaMBQxEjAQBgNVBAoTCUdv
+c2hhd2tEQjBZMBMGByqGSM49AgEGCCqGSM49AwEHA0IABFrAPcdlw5DWQmS9mCFX
+FlD6R8ABaBf4LA821wVmPa9tiM6n8vRJvbmHuSjy8LwJJRRjo9GJq7KD6ZmsK9P9
+sXijbTBrMA4GA1UdDwEB/wQEAwIHgDATBgNVHSUEDDAKBggrBgEFBQcDAjAMBgNV
+HRMBAf8EAjAAMBkGA1UdDgQSBBBX9qcbG4ofUoUTHGwOgGvFMBsGA1UdIwQUMBKA
+EL9sxrcr6QTwwk5csm2ZcfgwCgYIKoZIzj0EAwIDSAAwRQIgOK9PVJt7KdvDU/9v
+z9gQI8JnVLZm+6gsh6ro9WnaZ8YCIQDXhjfQAWaUmJNTgKq3rLHiEbPS4Mxl7h7S
+kbkX/2GIjg==
+-----END CERTIFICATE-----
+-----BEGIN EC PRIVATE KEY-----
+MHcCAQEEIN9Mf6CzDgCs1EbzJqDK3+12wcr7Ua3Huz6qNhyXCrS1oAoGCCqGSM49
+AwEHoUQDQgAEWsA9x2XDkNZCZL2YIVcWUPpHwAFoF/gsDzbXBWY9r22Izqfy9Em9
+uYe5KPLwvAklFGOj0YmrsoPpmawr0/2xeA==
+-----END EC PRIVATE KEY-----`
 )
 
 type TestInterface interface {
@@ -65,7 +92,7 @@ func (th *TestHelper) createConnections(num int, hosts []string) []*Connection {
 	results := make([]*Connection, num)
 	for i := 0; i < num; i++ {
 		host := hosts[i%len(hosts)]
-		conn, err := client.NewConnection(host, testUsername, []byte(testPassword))
+		conn, err := client.NewConnection(host, []byte(clientCertAndKeyPEM), []byte(clusterCertPEM))
 		if err != nil {
 			th.Fatal(err)
 		}
