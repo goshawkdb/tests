577571c65e5b34f221c0301df70f17221d785214
Matthew Sackman <matthew@goshawkdb.io>
2016-11-12 09:53:53 +0000

    Automate all incr topology changes.
    --HG--
    branch : dev

--- topology/incr/3/main.go
+++ topology/incr/3/main.go
@@ -20,17 +20,21 @@ func main() {
 
 	rm1 := setup.NewRM("one", 10001, nil, config1)
 	rm2 := setup.NewRM("two", 10002, nil, config2)
+	rm3 := setup.NewRM("three", 10003, nil, config2)
 
 	prog := h.Program([]h.Instruction{
 		setup,
 		rm1.Start(),
 		setup.Sleep(5 * time.Second),
 		rm2.Start(),
+		rm3.Start(),
 		setup.Sleep(15 * time.Second),
 		rm1.Terminate(),
 		rm2.Terminate(),
+		rm3.Terminate(),
 		rm1.Wait(),
 		rm2.Wait(),
+		rm3.Wait(),
 	})
 	log.Println(h.Run(setup, prog))
 
--- topology/incr/8/main.go
+++ topology/incr/8/main.go
@@ -19,18 +19,26 @@ func main() {
 	}
 
 	rm1 := setup.NewRM("one", 10001, nil, config1)
-	rm2 := setup.NewRM("two", 10002, nil, config2)
+	rm2 := setup.NewRM("two", 10002, nil, config1)
+	rm3 := setup.NewRM("three", 10003, nil, config1)
+	rm4 := setup.NewRM("four", 10004, nil, config2)
 
 	prog := h.Program([]h.Instruction{
 		setup,
 		rm1.Start(),
-		setup.Sleep(5 * time.Second),
 		rm2.Start(),
+		rm3.Start(),
+		setup.Sleep(5 * time.Second),
+		rm4.Start(),
 		setup.Sleep(15 * time.Second),
 		rm1.Terminate(),
 		rm2.Terminate(),
+		rm3.Terminate(),
+		rm4.Terminate(),
 		rm1.Wait(),
 		rm2.Wait(),
+		rm3.Wait(),
+		rm4.Wait(),
 	})
 	log.Println(h.Run(setup, prog))
 
