fc56a936da850485dc5345510160150df0697508
Matthew Sackman <matthew@goshawkdb.io>
2016-11-12 14:34:02 +0000

    Mechanise the decr topology tests too. This has revealed some issues in the server.
    --HG--
    branch : dev

---
+++ topology/decr/1/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	h "goshawkdb.io/tests/harness"
+	"log"
+	"syscall"
+	"time"
+)
+
+func main() {
+	setup := h.NewSetup()
+
+	config1, err := h.NewPathProvider("./v1.json", false)
+	if err != nil {
+		log.Fatal(err)
+	}
+	config2, err := h.NewPathProvider("./v2.json", false)
+	if err != nil {
+		log.Fatal(err)
+	}
+	configCopy, err := h.NewPathProvider("", false)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	rm1 := setup.NewRM("one", 10001, nil, configCopy)
+	rm2 := setup.NewRM("two", 10002, nil, configCopy)
+
+	prog := h.Program([]h.Instruction{
+		setup,
+
+		config1.CopyTo(setup.Dir, configCopy),
+		rm1.Start(),
+		rm2.Start(),
+
+		setup.Sleep(5 * time.Second),
+		config2.CopyTo(configCopy, configCopy),
+		rm2.Signal(syscall.SIGHUP),
+
+		setup.Sleep(15 * time.Second),
+		rm1.Terminate(),
+		rm2.Terminate(),
+		rm1.Wait(),
+		rm2.Wait(),
+	})
+	log.Println(h.Run(setup, prog))
+
+}
